de75bd3cbf3e9bca4daccf65fad9d954b3136c67
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2025-06-11 20:42:39 +0900

    added function to parse accepted q values

--- http/utils.go
+++ http/utils.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"strconv"
 	"strings"
 )
 
@@ -21,4 +22,31 @@ func GetHttpPathForFilepath(filepath string) string {
 	// Remove leading and suffix slashes
 	fp = strings.Trim(fp, "/")
 	return "/" + fp
+}
+
+func ParseAcceptedQValues(s string) (map[string]float64, error) {
+	retval := make(map[string]float64)
+	parts := strings.Split(s, ",")
+	for _, part := range parts {
+		part = strings.TrimSpace(part)
+		if part == "" {
+			continue
+		}
+		kv := strings.Split(part, ";")
+		if len(kv) == 1 {
+			retval[kv[0]] = 1.0
+		} else if len(kv) == 2 {
+			//parse q value
+			s = strings.TrimSpace(kv[1])
+			s = strings.TrimPrefix(s, "q=")
+			f, err := strconv.ParseFloat(s, 64)
+			if err != nil {
+				return nil, err
+			}
+			retval[kv[0]] = f
+		} else {
+			panic("error reading accepted values in header")
+		}
+	}
+	return retval, nil
 }
--- http/utils_test.go
+++ http/utils_test.go
@@ -1,6 +1,9 @@
 package http
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestGetHttpPathForFilepath(t *testing.T) {
 	type args struct {
@@ -27,6 +30,36 @@ func TestGetHttpPathForFilepath(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetHttpPathForFilepath(tt.args.filepath); got != tt.want {
 				t.Errorf("GetHttpPathForFilepath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseAcceptedQValues(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]float64
+		wantErr bool
+	}{
+		{"Empty string", args{""}, map[string]float64{}, false},
+		{"Valid entry", args{"deflate"}, map[string]float64{"deflate": 1.0}, false},
+		{"Valid entry with q value", args{"deflate;q=0.3"}, map[string]float64{"deflate": 0.3}, false},
+		{"Multiple entries", args{"deflate, gzip, br"}, map[string]float64{"deflate": 1.0, "gzip": 1.0, "br": 1.0}, false},
+		{"Multiple entries with q values", args{"deflate;q=1.0, gzip;q=0.3, br;q=0.1"}, map[string]float64{"deflate": 1.0, "gzip": 0.3, "br": 0.1}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseAcceptedQValues(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseAcceptedQValues() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseAcceptedQValues() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
